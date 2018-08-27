5ee0913d47cae7b069ad577b99b0194f88574508
unknown <tech2js@gmail.com>
2018-08-27 19:48:15 +0800

    added admin and auth middelware, and the models and routes

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const SALT_I = 10;
+require('dotenv').config();
 
 const userSchema = mongoose.Schema({
   email: {
@@ -37,7 +41,56 @@ const userSchema = mongoose.Schema({
   token: {
     type: String
   }
+});
+
+// tell app to hash password
+userSchema.pre('save', function(next) {
+  var user = this;
+
+  if(user.isModified('password')) {
+    bcrypt.genSalt(SALT_I, function(err, salt) {
+      if(err) return next(err);
+  
+      bcrypt.hash(user.password,salt, function(err, hash) {
+        if(err) return next(err);
+        user.password = hash;
+        next();
+      })
+    }) 
+  } else {
+    next();
+  }
 })
+
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if(err) return cb(err);
+    cb(null,isMatch)
+  })
+}
+
+userSchema.methods.generateToken = function(cb) {
+  var user = this;
+  var token = jwt.sign(user._id.toHexString(), process.env.SECRET);
+
+  user.token = token;
+  user.save(function(err,user) {
+    if(err) return cb(err);
+    cb(null,user);
+  })
+}
+
+userSchema.statics.findByToken = function(token, cb) {
+  var user = this;
+
+  jwt.verify(token, process.env.SECRET, function(err, decode) {
+    user.findOne({'_id': decode, 'token': token}, function(err,user) {
+      if(err) return cb(err);
+      cb(null, user);
+    })
+  })
+}
 
 const User = mongoose.model('User', userSchema);
 
--- server/server.js
+++ server/server.js
@@ -13,13 +13,78 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// modles
+// models
 const { User } = require('./models/user');
+const {  Brand } = require('./models/brand');
+const { Wood } = require('./models/wood');
+
+//middlewares
+const { auth } = require('./middleware/auth');
+const { admin } = require('./middleware/admin');
+
+//=================
+//     WOODS
+//=================
+app.post('/api/product/wood', auth, admin, (req, res ) => {
+
+  const wood = new Wood(req.body);
+
+  wood.save((err, doc) => {
+    if(err) return res.json({success: false, err});
+    res.status(200).json({
+      success: true,
+      wood: doc
+    })
+  })
+})
+
+app.get('/api/product/woods', (req, res) => {
+  Wood.find({}, (err, woods) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(woods)
+  })
+})
+
+
+//=================
+//      BRAND
+//=================
+app.post('/api/product/brand', auth,admin,(req,res)=> {
+  const brand = new Brand(req.body);
+
+  brand.save((err, doc) => {
+    if(err) return res.json({success: false, err});
+    res.status(200).json({
+      success: true,
+      brand: doc
+    })
+  })
+})
+
+app.get('/api/product/brands', (req, res) => {
+  Brand.find({}, (err, brands) => {
+    if(err) return res.status(400).send(err);
+    res.status(200).send(brands);
+  })
+})
 
 
 // ==============
-// USERS
+//     USERS
 // ==============
+
+app.get('/api/users/auth',auth,(req, res) => {
+  res.status(200).json({
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    cart: req.user.cart,
+    history: req.user.history
+  })
+})
 
 app.post('/api/users/register', (req, res) => {
   const user = new User(req.body);
@@ -29,13 +94,42 @@ app.post('/api/users/register', (req, res) => {
       return res.json({success: false, err});
       res.status(200).json({
         success: true,
-        userData: doc
       })
   });
 })
 
-const port = process.env.PORT || 3002;
+app.post('/api/users/login', (req, res) => {
+  //find the email
+  User.findOne({'email': req.body.email}, (err, user) => {
+    if(!user) return res.json({loginSuccess: false, message: 'Auth failed, email not found.'});
 
+  //check password
+    user.comparePassword(req.body.password,(err, isMatch) => {
+      if(!isMatch) return res.json({loginSuccess:false, message: 'Wrong password'})
+
+      user.generateToken((err,user) =>{
+        if(err) return res.status(400).send(err);
+        res.cookie('rr_auth', user.token).status(200).json({
+          loginSuccess: true
+        })
+      })
+    })
+  })
+});
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id},
+    { token: '' },
+    (err, doc) => {
+      if(err) return res.json({ sucees: false, err});
+      return res.status(200).send({
+        success: true
+      })
+    })
+})
+
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`App running at port ${port}`);
 })
